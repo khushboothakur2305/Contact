976ffdc4c4cce137cd23693e88053839a48845e0
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-11-08 02:37:18 +0530

    upadate

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Contacts } from "@ionic-native/contacts/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
-  constructor() {}
-
+  myContacts: any[] = [];
+  mobileNumber: any[] = [];
+  constructor(private contacts: Contacts) {}
+  loadContacts() {
+    let options = {
+      filter: "",
+      multiple: true,
+      hasPhoneNumber: true,
+    };
+    this.contacts.find(["*"], options).then((contacts) => {
+      this.myContacts = contacts;
+      this.myContacts.map((re) => {
+        this.mobileNumber.push(re.phoneNumbers[0].value);
+      });
+    });
+  }
 }
